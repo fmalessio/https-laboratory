c8f8b37d8a504ac3f33371ca44be9401134a9c97
Fede Alessio <alessiofederico@hotmail.com>
2019-09-21 12:58:45 -0300

    Strating project

---
+++ src/test/java/com/um/sistemasemergentes/httpslab/HttpslabApplicationTests.java
@@ -0,0 +1,16 @@
+package com.um.sistemasemergentes.httpslab;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class HttpslabApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
